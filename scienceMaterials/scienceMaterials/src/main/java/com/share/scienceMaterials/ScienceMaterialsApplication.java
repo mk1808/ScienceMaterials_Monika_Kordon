46d3e668adb16c7a955537a7c11ad5fd1004b9a7
mk1808 <monikor97@gmail.com>
2020-12-23 00:16:11 +0100

    wgranie projektu

---
+++ scienceMaterials/scienceMaterials/src/main/java/com/share/scienceMaterials/ScienceMaterialsApplication.java
@@ -0,0 +1,13 @@
+package com.share.scienceMaterials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScienceMaterialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScienceMaterialsApplication.class, args);
+	}
+
+}
---
+++ scienceMaterials/scienceMaterials/src/test/java/com/share/scienceMaterials/ScienceMaterialsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.share.scienceMaterials;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScienceMaterialsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
