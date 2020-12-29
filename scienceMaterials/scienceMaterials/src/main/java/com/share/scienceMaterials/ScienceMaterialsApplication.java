ef560fe908758bda3fca7bdbce7a6ea4cda89e4c
mk1808 <monikor97@gmail.com>
2020-12-29 21:26:35 +0100

    projekt

--- scienceMaterials/scienceMaterials/src/main/java/com/share/scienceMaterials/ScienceMaterialsApplication.java
+++ scienceMaterials/scienceMaterials/src/main/java/com/share/scienceMaterials/ScienceMaterialsApplication.java
@@ -1,9 +1,11 @@
 package com.share.scienceMaterials;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.boot.autoconfigure.jdbc.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ScienceMaterialsApplication {
 
 	public static void main(String[] args) {
