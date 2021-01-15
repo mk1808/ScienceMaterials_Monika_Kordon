d10359e4d910c958c02080773e13ad2b1dbea576
mk1808 <monikor97@gmail.com>
2021-01-15 02:05:25 +0100

    #6 poprawa wysylania i pobieraia plikow

--- scienceMaterials/scienceMaterials/src/main/js/view/app/src/components/common/file/fileDirective.js
+++ scienceMaterials/scienceMaterials/src/main/js/view/app/src/components/common/file/fileDirective.js
@@ -2,7 +2,7 @@
 
 angular
     .module('materialsApp')
-    .directive('fileDirective', function () {
+    .directive('fileDirective', ['ArticleRestService', function (articleRestService) {
         return {
             restrict: 'E',
             templateUrl: '/src/components/common/file/file.html',
@@ -10,7 +10,25 @@ angular
                 file: '=',
             },
             link: function ($scope, element, attrs) {
+                var scope=$scope;
+                $scope.PATH = 'http://localhost:8080/api/articles/files/'+scope.file.source;
                 console.log($scope.file)
+                
+                $scope.download = function(){
+                    debugger;
+                    articleRestService.getFile(scope.file.source, scope.success, scope.error);
+
+                }
+
+                $scope.success = function(response){
+                    console.log(response) 
+                  
+                    
+                }
+                $scope.error = function(response){
+                   console.log(response)
+                }
             }
         };
-    });
+    }]
+    );
